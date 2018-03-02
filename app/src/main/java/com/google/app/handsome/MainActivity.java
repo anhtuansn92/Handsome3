8aee53e85e428806f624133d8f19cf07d89717cc
anhtuansn92 <anhtuansn92@gmail.com>
2018-03-01 22:14:01 -0800

    lan dau up code len github

---
+++ app/src/main/java/com/google/app/handsome/MainActivity.java
@@ -0,0 +1,27 @@
+package com.google.app.handsome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnSearch = (Button) findViewById(R.id.btn_search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
+
+    }
+}
